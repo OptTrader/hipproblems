64fd2c0bdcded09b0addc81fba53956f2a03e6f1
Chris Kong <christopher.j.kong@gmail.com>
2017-05-16 14:15:05 -0700

    Added color schemes files

---
+++ ios_hotelapp/Hotelzzz/Hotelzzz/ColorScheme.swift
@@ -0,0 +1,18 @@
+//
+//  ColorScheme.swift
+//  Hotelzzz
+//
+//  Created by Chris Kong on 5/16/17.
+//  Copyright © 2017 Hipmunk, Inc. All rights reserved.
+//
+
+import UIKit
+
+struct ColorScheme {
+    static let navigationBarForegroundColor = UIColor.colorWithHex(hex: "00B6E8")
+    static let navigationBarBackgroundColor = UIColor.black
+    static let primaryBackgroundColor = UIColor.colorWithHex(hex: "01090F")
+    static let cellSeparatorColor = UIColor(white: 1, alpha: 0.2)
+    static let cellPrimaryTextColor = UIColor.colorWithHex(hex: "00B6E8")
+    static let cellSecondaryTextColor = UIColor.colorWithHex(hex: "7498A6")
+}
